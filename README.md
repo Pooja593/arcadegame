469100fa7314094970428cfc215502d0c8c0f011
Pooja593 <mpooja1062000@gmail.com>
2019-06-20 16:53:40 +0530

    success

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# arcadegame
+# Classic Arcade Game Clone Project
+
+## Table of Contents
+
+- [Instructions](#instructions)
+- [Contributing](#contributing)
+
+## Instructions
+
+Use this [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking your submission.
+
+Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
+
+Your **README.md** file should be updated with instructions on both how to 1. Run and 2. Play your arcade game.
+
+For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+## Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+## I followed the below steps to develop Game.
++ I had downloaded the skeleton project from GitHub link, provided by Udacity through https://github.com/udacity/frontend-nanodegree-arcade-game.git.
++ After that I extracted the downloaded zip file.
++ I had read all the modules and viewed all the images.
++ I had opened index.html using browser to test the output.
++ First I have rectified some errors to get canvas.
++ Then I got enemies on canvas and hero named as `warrior` on canvas after rectifying some more errors
++  I had implemented the below functions to `warrior`.
+  - `render()`
+  - `update()` and
+  - `handleInput()`
++ `render()` is for placing a warrior on the canvas, `update()` is for updating the player position. This function already implemented by using a `dt` parameter.
++ `handleInput()` is to handle the inputs which was given by user. The input indications were already given. I implemented the functionality by using the indications.
+Here the indications are:
+  - left arrow: when this key is pressed then the `warrior` moves towards left side of x-axis.
+  - up arrow: when this key is pressed the `warrior` moves upwards along y-axis.
+  - right arrow: when this key is pressed the `warrior` moves towards right side of x-axis.
+  - down arrow: when this key is pressed the `warrior` moves downwards along y-axis.
++ I had also implemented some functions to enemy like
+  - `render()` and
+  - `update()`.
++ Then I had given `speed` using `random` function for enemies(bugs).
++ I had given the boundaries to Warrior so that he cannot move beyond the canvas.
++ Here the warrior will have 5 lives to score maximum.
++ The score increases when `warrior` touches the water area.
++ The warrior changes his character to another character, `char-horn-girl` to `char-pink-girl` when he touches the water area.
++ the background color of the screen also changes when `warrior` touches the water.
++ After five lives the Game will complete with the pop-up alert showing as game over and displays points score within five lives.
++ When `warrior` click ok button on pop-up then the game will start again by initializing points as 0 and lives as five `heart` images.
+## how to play game
++ please open index.html in browser.
++ Here user can move the `warrior` by arrow keys.
++ on touching water area the user can gain score.
++ If `warrior` collide with bugs(enemies) then he will loose lives.
++ If `warrior` loose all the lives then the game will complete the a pop-up alert.
++ The game will start again by clicking ok button on pop-up alert.
---
+++ js/app.js
@@ -0,0 +1,117 @@
+var score = 0;
+var collision = 0;
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
+	// Variables applied to each of our instances go here,
+	// we've provided one for you to get started
+	this.x = x;
+	this.y = y;
+	this.speed = speed;
+	// The image/sprite for our enemies, this uses
+	// a helper we've provided to easily load images
+	this.sprite = 'images/enemy-bug.png';
+};
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+	// You should multiply any movement by the dt parameter
+	// which will ensure the game runs at the same speed for
+	// all computers.
+	this.x = this.x + this.speed * dt;
+	if (this.x == 0) {
+		this.speed = 100 + Math.floor(Math.random() * 400);
+	}
+	if (this.x > 500) {
+		this.speed = 100 + Math.floor(Math.random() * 400);
+		this.x = 0;
+	}
+	if (player.x + 70 > this.x && player.x < this.x + 70 && player.y + 70 > this.y && player.y < this.y + 70) {
+		collision = collision + 1;
+		player.x = 200;
+		player.y = 400;
+	}
+	if (collision == 1) {
+		document.getElementById('image1').style.visibility = "hidden";
+	} else if (collision == 2) {
+		document.getElementById('image2').style.visibility = "hidden";
+	} else if (collision == 3) {
+		document.getElementById('image3').style.visibility = "hidden";
+	} else if (collision == 4) {
+		document.getElementById('image4').style.visibility = "hidden";
+	} else if (collision == 5) {
+		document.getElementById('image5').style.visibility = "hidden";
+	} else if (collision > 5) {
+		alert("gameOver startAgain!!!");
+		window.location.reload();
+		collision = 0;
+		score = 0;
+		document.getElementById('points').innerHTML = score;
+	}
+}
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+class Warrior {
+	constructor(x, y, sprite) {
+		this.x = x;
+		this.y = y;
+		this.sprite = 'images/char-horn-girl.png';
+	}
+}
+Warrior.prototype.update = function(dt) {}
+Warrior.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [];
+var enemyPlaces = [61, 144, 227];
+enemyPlaces.map((positionY) => {
+	var enemyBug = new Enemy(0, positionY, 150);
+	allEnemies.push(enemyBug);
+})
+var player = new Warrior(200, 400);
+Warrior.prototype.handleInput = function(key) {
+	if (key == 'left') {
+		this.x = this.x - 101;
+		if (this.x < 0) {
+			this.x = 0;
+		}
+	} else if (key == 'right') {
+		this.x = this.x + 101;
+		if (this.x > 404) this.x = 404;
+	} else if (key == 'up') {
+		this.y = this.y - 82;
+		if (this.y < 60) {
+			this.sprite = 'images/char-pink-girl.png';
+			document.body.style.backgroundColor = "#d7f9d7";
+			score = score + 100;
+			document.getElementById('points').innerHTML = score;
+			setTimeout(() => {
+				this.x = this.x;
+				this.y = 400;
+				document.body.style.backgroundColor = "#fff";
+				this.sprite = 'images/char-horn-girl.png';
+			}, 110)
+		}
+	} else if (key == 'down') {
+		this.y = this.y + 82;
+		if (this.y > 400) this.y = 400;
+	}
+}
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+	var allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down'
+	};
+	player.handleInput(allowedKeys[e.keyCode]);
+});
